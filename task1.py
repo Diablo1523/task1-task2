0936b30ba92bd08fdd9571630e247c04d4d59f5b
Diablo1523 <diablobruh1523@gmail.com>
2025-07-11 01:14:27 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+x=input('Enter the first number: ')
+y=input('Enter the seconed number: ')
+x=int(x)
+y=int(y)
+a=x+y
+s=x-y
+m=x*y
+d=x/y
+
+print('Addition: ' , a)
+print('Substraction: ' , s)
+print('Multiplication' , m)
+print('division' , d)
+
---
+++ task2.py
@@ -0,0 +1,6 @@
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+
+c=a+" "+b
+
+print(f"\nHello, {c}! Welcome to the python program." )
